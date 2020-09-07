bb8550d6372cb33f2acfd9422642db5529a41aa3
Brandon Courtney <brandon.h.courtney@gmail.com>
2020-09-07 11:42:18 -0500

    Edit

--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -6,7 +6,6 @@ import Header from "../header"
 describe("Sanity test", () => {
   it("will pass", () => {
     console.log("scalloped potatoes::: 8")
-    console.log("scalloped potatoes::: 9")
     expect(true).toBe(true)
   })
 })
