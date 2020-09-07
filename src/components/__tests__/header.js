61c3aa02f673cb6138031b44f830ff73f4778817
Brandon Courtney <brandon.h.courtney@gmail.com>
2020-09-07 09:38:42 -0500

    Sets up basic testing with Jest

---
+++ src/components/__tests__/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Header from "../header"
+
+describe("Sanity test", () => {
+  it("will pass", () => {
+    console.log("scalloped potatoes::: 8")
+    expect(true).toBe(true)
+  })
+})
