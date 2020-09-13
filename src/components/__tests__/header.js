cef374d3d9c4427f69f638213787600a1569ebf8
Brandon Courtney <brandon.h.courtney@gmail.com>
2020-09-13 12:50:19 -0500

    update travis file to test "script" section & test if "echo" will display

--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -6,6 +6,6 @@ import Header from "../header"
 describe("Sanity test", () => {
   it("will pass", () => {
     console.log("scalloped potatoes::: Failing test")
-    expect(true).toBe(false)
+    expect(true).toBe(true)
   })
 })
